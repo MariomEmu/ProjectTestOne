a3fe08c94e3b41747e6635b9f3e9489d29a51e8b
hadiChowdhury <chowdhuryhadiaiub20@gmail.com>
2023-04-05 21:54:31 +0600

    new model created

---
+++ ProjectTestOne/Models/Admin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectTestOne.Models
+{
+    public class Admin
+    {
+        public int id { get; set; }
+    }
+}
